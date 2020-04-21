3c4c6be1b0d764ba73cde2ca1bd580909bb729fb
V1n1c1us <v.f.diehl@gmail.com>
2020-04-21 20:14:42 -0300

    Search github repositories with axios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,69 +1,65 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
-
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepositories(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input type="text" placeholder="Digite aqui" />
+      <Form onSubmit={handleAddRepositories}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite aqui"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/1993081?s=460&u=63c9a59beb3567b3b7aae98960ba695b47ddc3b8&v=4"
-            alt="Profile"
-          />
-          <div>
-            <strong>github/V1n1c1us</strong>
-            <p>
-              Seeing something unexpected? Take a look at the GitHub profile
-              guide 🚀.
-            </p>
-          </div>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/1993081?s=460&u=63c9a59beb3567b3b7aae98960ba695b47ddc3b8&v=4"
-            alt="Profile"
-          />
-          <div>
-            <strong>github/V1n1c1us</strong>
-            <p>
-              Seeing something unexpected? Take a look at the GitHub profile
-              guide 🚀.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/1993081?s=460&u=63c9a59beb3567b3b7aae98960ba695b47ddc3b8&v=4"
-            alt="Profile"
-          />
-          <div>
-            <strong>github/V1n1c1us</strong>
-            <p>
-              Seeing something unexpected? Take a look at the GitHub profile
-              guide 🚀.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
